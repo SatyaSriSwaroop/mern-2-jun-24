b5e3fadd45d8450ab60ff5fe4121d4fdc276317b
KasarapuSatyaSriSwaroop <satyasriswaroop97@gmail.com>
2024-06-22 09:34:34 +0530

    session6

--- session5/routes/blogs.routes.js
+++ session5/routes/blogs.routes.js
@@ -1,7 +1,14 @@
 const express = require("express");
-const {postNewBlog} = require("../controllers/blogs.controllers");
+const {postNewBlog, getBlogs, deleteBlog, getBlogById, updateBlogById} = require("../controllers/blogs.controllers");
 const router = express.Router();
 
+router.get("/", getBlogs);
+router.get("/:id", getBlogById);
 router.post("/", postNewBlog);
+router.patch("/:id", updateBlogById); //when only few fields we want to update, we use patch.
+router.delete("/:id", deleteBlog);
+// router.post("/delete/:id", deleteBlog); //we can do like this also but its better to follow http guidlines and use diff methods.
+
+
 
 module.exports = router;
